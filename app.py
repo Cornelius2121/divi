fd5f8784eeecc7b4e062f570cee9e6e1233536b4
Cornelius Paardekooper <67299665+Cornelius2121@users.noreply.github.com>
2023-09-07 14:22:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, make_response, jsonify
+import sys
 
 app = Flask(__name__)
+app.secret_key = 'any random string'
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
+
+@app.route('/create', methods=['POST'])
+def create():
+    content = request.json
+    session['rules'] = content
+    # function call
+    response = make_response(jsonify({
+        'message': 'success'
+    }), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
