96a0ce9f50785688c83d59ca1d9d36e3484de29e
Cornelius Paardekooper <67299665+Cornelius2121@users.noreply.github.com>
2023-09-07 14:50:19 +1000

    Makes assignment call

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, make_response, jsonify
 import sys
+from helper.assignment import assignPeople, Person, AssignmentParams
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
@@ -14,12 +15,14 @@ def hello_world():  # put application's code here
 def create():
     content = request.json
     session['rules'] = content
-    # function call
+    params = AssignmentParams(1)
+    x = assignPeople(people=[Person(fname, 'x') for fname in content[0]['people']], params=params)
     response = make_response(jsonify({
         'message': 'success'
     }), 200)
     response.headers["Content-Type"] = "application/json"
     return response
+
 
 if __name__ == '__main__':
     app.run(port=8000)
