7fac5859af92aaf02da5e8b74a2b8ea6aa09d260
Tim Farnworth <timothy.farnworth@uon.edu.au>
2023-09-07 15:44:08 +1000

    Fix unit tests running from terminal

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-
+import os, sys
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
 from helper.assignment import assignPeople, Person, AssignmentParams
 
 
