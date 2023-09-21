89b1c8411e4bc0177870f580c1186f65e1e8efb8
Tim Farnworth <timothy.farnworth@uon.edu.au>
2023-09-21 15:01:39 +1000

    Add testing of saving yaml

---
+++ tests/test_assignment_yaml.py
@@ -0,0 +1,33 @@
+import unittest
+import os, sys
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+from helper.assignment import AssignmentParams, Person, assignPeople, saveAssignmentYaml, loadAssignmentYaml
+
+
+class TestSaveToYaml(unittest.TestCase):
+    def test_can_save_as_yaml(self):
+        params = AssignmentParams(1, this_year=2021)
+        p1 = Person("Tim", "Smith")
+        p2 = Person("Jon", "Scott")
+        p3 = Person("Cornelius", "Spence")
+        p4 = Person("Macy", "Spence")
+        p1.addPersonHasBeenOn(p2, 2020)
+        p2.addPersonHasBeenOn(p3, 2019)
+        p3.addPersonHasBeenOn(p1, 2018)
+        p4.addPersonHasBeenOn(p1, 2019)
+        p3.addPersonThatCantBeOn(p4)
+        p4.addPersonThatCantBeOn(p3)
+        people = [p1, p2, p3, p4]
+
+        people = assignPeople(people=people, params=params)
+        saveAssignmentYaml(people, f"{parent_dir}/tests/data/assignments_{params.this_year}.yaml", params.this_year)
+
+    def test_load_assignments_2021(self):
+        year = 2021
+        people = loadAssignmentYaml(f"{parent_dir}/tests/data/assignments_{year}.yaml")
+
+
+if __name__ == '__main__':
+    unittest.main()
