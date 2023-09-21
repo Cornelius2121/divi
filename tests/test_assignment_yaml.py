1edbbad6d9fa592c9a7a6f30b890915b2967c6a0
Tim Farnworth <timothy.farnworth@uon.edu.au>
2023-09-21 15:03:22 +1000

    Bug fix

--- tests/test_assignment_yaml.py
+++ tests/test_assignment_yaml.py
@@ -3,7 +3,7 @@ import os, sys
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
-from helper.assignment import AssignmentParams, Person, assignPeople, saveAssignmentYaml, loadAssignmentYaml
+from helper.assignment import AssignmentParams, Person, assignPeople, saveAssignmentYaml
 
 
 class TestSaveToYaml(unittest.TestCase):
